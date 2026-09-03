ee259475755b86fe0b14d6fea9655bf2ced08dc0
pokek0 <sr.olono@gmail.com>
2026-09-02 23:21:35 -0700

    Add assets configuration file for importing images from src directory

---
+++ src/assets.ts
@@ -0,0 +1,13 @@
+// Importar imágenes desde /src
+import logo from './1_20260902_230633_0000.jpg';
+import chapter1 from './2_20260902_230633_0001.jpg';
+import chapter2 from './3_20260902_230633_0002.jpg';
+import chapter3 from './4_20260902_230633_0003.jpg';
+import chapter4 from './5_20260902_230633_0004.jpg';
+
+export const ASSETS = {
+  logo,
+  chapters: [chapter1, chapter2, chapter3, chapter4]
+};
+
+export default ASSETS;
